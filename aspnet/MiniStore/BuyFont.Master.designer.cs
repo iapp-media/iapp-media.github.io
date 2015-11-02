56f4ba34cb094986e21ff2e5c87cdd2f85536dc1
jasonzhang80 <isotopme@gmail.com>
2015-11-02 21:14:19 +0800

    登出鈕

--- aspnet/MiniStore/BuyFont.Master.designer.cs
+++ aspnet/MiniStore/BuyFont.Master.designer.cs
@@ -49,6 +49,15 @@ namespace MiniStore {
         protected global::System.Web.UI.WebControls.Literal L_MyStore;
         
         /// <summary>
+        /// LBLogout 控制項。
+        /// </summary>
+        /// <remarks>
+        /// 自動產生的欄位。
+        /// 將移動欄位宣告從設計檔案修改為程式碼後置檔案。
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.LinkButton LBLogout;
+        
+        /// <summary>
         /// ContentPlaceHolder1 控制項。
         /// </summary>
         /// <remarks>
