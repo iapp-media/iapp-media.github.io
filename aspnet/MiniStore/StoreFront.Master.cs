4d4968a8657d852c4aab437127203d90b7faf127
LeoZe <hiimzeyan@gmail.com>
2015-11-06 16:10:49 +0800

    dll

--- aspnet/MiniStore/StoreFront.Master.cs
+++ aspnet/MiniStore/StoreFront.Master.cs
@@ -44,8 +44,9 @@ namespace MiniStore
                 LCarLink.Text = " <a href=\"Buy_Ctrl.aspx?SN=" + Request.QueryString["SN"] + "\"> <img class=\"back-top\" src=\"img/cart.png\" /> </a>";
 
                 jump = "../Login/m-login.aspx?s=1&done=" + HttpUtility.UrlEncode("../StoreMana/default.aspx");
-                L_MyStore.Text = " <li><a href='" + jump + "'  target='_self' >打造自己的微店</a></li>" +
-                                 " <li><a href='#'>加入行動分店</a></li>" +
+               // L_MyStore.Text = " <li><a href='" + jump + "'  target='_self' >打造自己的微店</a></li>" +
+                L_MyStore.Text = " <li><a href='javascript:void(0)'  onclick='join()' target='_self' >打造自己的微店</a></li>" +
+                                 " <li><a href='javascript:void(0)'  onclick='JoinBranch()' >加入行動分店</a></li>" + 
                                  " <li class='SandTitle'>我的帳戶</li>" +
                                  " <li><a href='../Login/me/m-profile.aspx?done=" + HttpUtility.UrlEncode("../../MiniStore/default.aspx?SN=" + Request.QueryString["SN"]) + "'> 編輯會員資料</a></li> " +
                                  " <li><a href='Order_history.aspx?SN=" + Request.QueryString["SN"] + "'>訂單查詢</a></li>";
