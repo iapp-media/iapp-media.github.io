d1fa972a6664f0b476567caf315c3c68e778e89e
jasonzhang80 <isotopme@gmail.com>
2015-11-12 15:37:18 +0800

    換方法

---
+++ aspnet/MiniStore/JoinAs.aspx
@@ -0,0 +1,27 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="JoinAs.aspx.cs" Inherits="MiniStore.JoinAs" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <%--        class="JoinBox"--%>
+            <div class="LOGO">
+                <img src="img/img-1.jpg" alt="Alternate Text" />
+            </div>
+            <div class="Sendbox">
+                <p>輸入您的微店店名</p>
+                <asp:TextBox ID="TB_SNAME" runat="server"></asp:TextBox>
+                <div>
+                    <asp:Button ID="BT_SNAME" runat="server" Text="確定" CssClass="btn btn-warning sendcareButtomeEnd" OnClick="BT_SNAME_Click" />
+                </div>
+            </div>
+        </div>
+    </form>
+</body>
+</html>
---
+++ aspnet/MiniStore/JoinAs.aspx.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace MiniStore
+{
+    public partial class JoinAs : System.Web.UI.Page
+    {
+        CommTool Comm = new CommTool();
+        JDB Main = new JDB();
+        string str = "";
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            { 
+                string SID = "";
+                SID = Main.Scalar("select idno from Store where User_ID='" + Comm.User_ID() + "'");
+                if (SID != "")
+                { 
+                    int c = Comm.DeleCoookie("iapp_sid"); //暫時確保 1106
+                    int a = Comm.SaveCookie("iapp_sid", SID, 365);
+
+                    Response.Redirect(HttpUtility.UrlDecode(HttpUtility.UrlEncode("../StoreMana/default.aspx"))); 
+                }
+                else
+                {
+                    //Response.Write("join");
+                } 
+            }
+        }
+        protected void BT_SNAME_Click(object sender, EventArgs e)
+        {
+            if (Comm.User_ID() != -1)
+            {
+                Main.ParaClear();
+                Main.ParaAdd("@UID", Comm.User_ID(), System.Data.SqlDbType.Int);
+
+                Main.NonQuery("Insert into Store (User_ID,Creat_Date) values " +
+                 " (@UID,getdate())   ");
+                string SID = "";
+                SID = Main.Scalar("select IDNo from Store where User_ID='" + Comm.User_ID() + "'");
+                if (SID != "")
+                { 
+                    Main.ParaClear();
+                    Main.ParaAdd("@SID", Main.Cint2(SID), System.Data.SqlDbType.Int);
+                    Main.ParaAdd("@Store_No", Comm.StoreSN(Main.Cint2(SID)), System.Data.SqlDbType.NVarChar);
+                    Main.ParaAdd("@Store_Name", TB_SNAME.Text, System.Data.SqlDbType.NVarChar);
+                    Main.NonQuery("update Store set Store_No=@Store_No where idno=@SID");
+                    Main.NonQuery("insert into Store_info (Store_ID,Store_Name) values(@SID,@Store_Name)");
+                    Comm.SaveCookie("iapp_sid", SID);
+                }
+                if (SID != "")
+                {
+                    Response.Redirect(HttpUtility.UrlDecode(HttpUtility.UrlEncode("../StoreMana/default.aspx")));
+                }
+            }
+        }
+
+    }
+}
