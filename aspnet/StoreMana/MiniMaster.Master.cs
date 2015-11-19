3bc6f559cfcccaf4449c15d848d6fe34bb0876b0
jasonzhang80 <isotopme@gmail.com>
2015-11-19 13:45:18 +0800

    開店判斷

--- aspnet/StoreMana/MiniMaster.Master.cs
+++ aspnet/StoreMana/MiniMaster.Master.cs
@@ -14,17 +14,22 @@ namespace StoreMana.Mini
         protected void Page_Load(object sender, EventArgs e)
         {
             if (!IsPostBack)
-            {
-              //  Comm.DeleCoookie("iapp_sid");
-            
-              //      Comm.SaveCookie("iapp_sid", "4");
-               
+            { 
 
                 if (Comm.Store_ID() == -1)
                 {
                     Response.Write("<Script>window.open('" + "../Login/m-login.aspx?s=1&done=" + HttpUtility.UrlEncode("../StoreMana/default.aspx") + "','_self')</Script>");
                 }
-                string SNID = Main.Scalar("Select Store_NID from Store where idno='" + Comm.Store_ID() + "'");
+
+                Main.ParaClear();
+                Main.ParaAdd("@SID", Comm.Store_ID(), System.Data.SqlDbType.Int);
+                if (Main.Scalar("Select isnull(Store_Cate,0) from Store_info where IDNo=@SID") == "0")
+                {
+                    Response.Redirect("ThreeOpen.aspx");
+                    return;
+                }
+
+                string SNID = Main.Scalar("Select Store_NID from Store where idno=@SID");
                 menu_QR.Text = "<a href=\"" + Comm.MiStoreUrl + "Default.aspx?SN=" + SNID + "&Intr=" + Comm.User_ID() + "\" target=\"_blank\" ><img src=\"QRcode.ashx?t=" + Comm.MiStoreUrl + "Default.aspx?SN=" + SNID + "&Intr=" + Comm.User_ID() +"\" alt=\"\" class=\"QRcode\"> </a>";
                 LGoMini.Text = "<li><a href=\"../../MiniStore/Default.aspx?SN=" + SNID + "\"><span class=\"glyphicon glyphicon-home\" aria-hidden=\"true\"></span>回iApp微店</a></li>";
             }
