87713354f909d90258909e34e3a90228fccb8e14
jasonzhang80 <isotopme@gmail.com>
2015-11-27 17:48:59 +0800

    111112

--- aspnet/MiniStore/BuyFont.Master.cs
+++ aspnet/MiniStore/BuyFont.Master.cs
@@ -40,8 +40,13 @@ namespace MiniStore
                 menu_QR.Text = "<a href=\"Default.aspx?SN=" + Request.QueryString["SN"] + "&Intr=" + Comm.User_ID() + "\" target=\"_blank\" ><img src=\"QRcode.ashx?t=" + Comm.URL + "Default.aspx?SN=" + Request.QueryString["SN"] + "&Intr=" + Comm.User_ID() + "\" alt=\"\" class=\"QRcode\"> </a>" +
                     " <a href=\"http://line.naver.jp/R/msg/text/?" + Comm.URL + "Default.aspx?SN=" + Request.QueryString["SN"] + "\">" +
                               "  <img src=\"img/lineicon.png\" alt=\"用LINE傳送\" class=\"line\" />  </a>";
-               
-                LSinfo.Text = "<li><a href=\"SInfo.aspx?SN=" + Request.QueryString["SN"] + "\">微店資訊</a></li>";
+                Main.ParaClear();
+                Main.ParaAdd("@SN", Request.QueryString["SN"].ToString(), System.Data.SqlDbType.NVarChar);
+
+                LSinfo.Text = "<li> " + Main.Scalar("Select Store_Name from Store_info where Store_ID in (select IDNo from Store where Store_NID=@SN )") + " </li>" +
+                    "<li><a href=\"SInfo.aspx?SN=" + Request.QueryString["SN"] + "\">微店資訊</a></li>" +
+                    "<li><a href='Order_history.aspx?SN=" + Request.QueryString["SN"] + "'>購買紀錄</a></li>" +
+                    "<a class=\"navbar-brand\" href=\"default.aspx?SN=" + Request.QueryString["SN"] + "\"><li>回首頁</li></a>";
 
                 //LCarLink.Text = " <a id=\"Buycar\"  href=\"Buy_Ctrl.aspx?SN=" + Request.QueryString["SN"] + "\">" +
                 //    " <img class=\"back-top\" src=\"img/cart.png\" /><span/>" +
@@ -53,14 +58,13 @@ namespace MiniStore
 
                  
 
-                Store_Name.Text = "<a class=\"navbar-brand\" href=\"default.aspx?SN=" + Request.QueryString["SN"] + "\"> <img class=\"iapplogo\" src=\"img/ministorelogo.png\" />" +
-           " </a><a href=\"SInfo.aspx?SN=" + Request.QueryString["SN"] + "\"><div><h3 class=\"FixTitle\">" + Main.Scalar("Select Store_Name from Store_info where Store_ID in (select IDNo from Store where Store_NID=@SN )") + "</h3></div></a>";
+                Store_Name.Text = "  <img class=\"iapplogo\" src=\"img/ministorelogo.png\" />" +
+           "  <a href=\"SInfo.aspx?SN=" + Request.QueryString["SN"] + "\"><div><h3 class=\"FixTitle\">" + Main.Scalar("Select Store_Name from Store_info where Store_ID in (select IDNo from Store where Store_NID=@SN )") + "</h3></div></a>";
 
 
 
                 L_MyStore.Text = " <li class='SandTitle'>我的帳戶</li>" +
-                      " <li><a href='../Login/me/m-profile.aspx?done=" + HttpUtility.UrlEncode("../../MiniStore/default.aspx?SN=" + Request.QueryString["SN"]) + "'> 個人檔案</a></li> " +
-                      " <li><a href='Order_history.aspx?SN=" + Request.QueryString["SN"] + "'>購買紀錄</a></li>";
+                      " <li><a href='../Login/me/m-profile.aspx?done=" + HttpUtility.UrlEncode("../../MiniStore/default.aspx?SN=" + Request.QueryString["SN"]) + "'> 個人檔案</a></li> " ;
 
 
             }
