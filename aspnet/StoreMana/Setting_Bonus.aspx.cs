fcce4f77d37141e0dc0187b175865df308f4cfc9
zeyan0220 <hiimzeyan@gmail.com>
2015-12-16 10:15:31 +0800

    jason

---
+++ aspnet/StoreMana/Setting_Bonus.aspx.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data;
+
+namespace StoreMana
+{
+    public partial class Setting_Bonus : System.Web.UI.Page
+    {
+        JDB Main = new JDB();
+        CommTool Comm = new CommTool();
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                Main.ParaClear();
+                Main.ParaAdd("@Store_ID", Main.Cint2(Session["Store_ID"].ToString()), SqlDbType.Int);
+
+                if (Main.Scalar("select 1 from Store_Bonus where Store_ID=@Store_ID") != "")
+                {
+                    DataTable DT = Main.GetDataSetNoNull("select * from Store_Bonus");
+                    if (DT.Rows.Count > 0)
+                    {
+                        if ( DT.Rows[0]["Isable"].ToString()  =="1") { ISBonus.Checked = true;   } else { ISBonus.Checked = false; }
+                        TBPoint.Text = DT.Rows[0]["Bpoint"].ToString();
+                        TBPrice.Text = DT.Rows[0]["Discount"].ToString();
+                    }
+                }
+            }
+        }
+
+        protected void BTSave_Click(object sender, EventArgs e)
+        {
+            string tmp = "";
+            int Isable = 0;
+            if (ISBonus.Checked == true) { 
+                Isable = 1;
+                if (TBPoint.Text == "") { tmp += ",折扣點數"; }
+                if (TBPrice.Text == "") { tmp += ",折扣金額"; }
+            } else { Isable = 0; }
+
+            if (tmp != "")
+            {
+                this.ClientScript.RegisterStartupScript(this.GetType(), "String", "<script>alert('請輸入" + tmp + "');</script>");
+                return;
+            }
+
+            Main.ParaClear();
+            Main.ParaAdd("@Store_ID", Main.Cint2(Session["Store_ID"].ToString()), SqlDbType.Int);
+            Main.ParaAdd("@Isable", Isable, SqlDbType.Int);
+            Main.ParaAdd("@Bpoint", Main.Cint2(TBPoint.Text), SqlDbType.Int);
+            Main.ParaAdd("@Discount", Main.Cint2(TBPrice.Text), SqlDbType.Int);
+
+            Main.NonQuery("if not exists (select 1 from Store_Bonus where Store_ID=@Store_ID) " +
+                          "insert into Store_Bonus(Store_ID, Isable, Bpoint, Discount) values " +
+                          "(@Store_ID, @Isable, @Bpoint, @Discount) else " +
+                          "update Store_Bonus set Isable=@Isable, Bpoint=@Bpoint, Discount=@Discount where Store_ID=@Store_ID");
+        }
+    }
+}
---
+++ aspnet/StoreMana/obj/Release/Package/PackageTmp/Setting_Bonus.aspx
@@ -0,0 +1,32 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/MiniMaster.Master" AutoEventWireup="true" CodeBehind="Setting_Bonus.aspx.cs" Inherits="StoreMana.Setting_Bonus" %>
+
+<asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
+    <div id="Allswiper">
+        <div class="swiper-container">
+            <div class="swiper-wrapper">
+                <div class="swiper-slide"><a href="Setting.aspx" style="color: white">參數設定</a></div>
+                <div class="swiper-slide"><a href="Setting_Bonus.aspx" style="color: white">點數設定</a></div>
+                <div class="swiper-slide"><a href="Setting_SInfo.aspx" style="color: white">微店管理</a></div>
+                <div class="swiper-slide"><a href="Setting_Gerent.aspx" style="color: white">帳號管理</a></div>
+            </div>
+        </div>
+    </div>
+    <div class="buydivmove">
+        <div class="col-xs-12 insidecare">
+            <div class="row">
+                <div class="col-xs-12  BTbox">
+                    <asp:CheckBox ID="ISBonus" runat="server" />
+                    <p class="BTleft">啟用點數活動</p>
+                </div>
+                <div class="col-xs-12 BTbox">
+                    <p class="BTleft">點數折率:</p>
+                    <asp:TextBox ID="TBPoint" runat="server" placeholder="1"></asp:TextBox>點折<asp:TextBox ID="TBPrice" runat="server" placeholder="1"></asp:TextBox>元 
+                    <p style="color: red;">折扣上限為訂單金額10%</p>
+                </div> 
+                <asp:Button ID="BTSave" runat="server" Text="儲存" CssClass="btn btn-warning SBuyCar2" OnClick="BTSave_Click" />
+            </div>
+        </div>
+    </div>
+</asp:Content>
