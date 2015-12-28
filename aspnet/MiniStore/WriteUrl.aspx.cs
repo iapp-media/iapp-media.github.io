849c16bbc0b02df075647b5f326f7c0aa2a28765
zeyan0220 <hiimzeyan@gmail.com>
2015-12-28 10:22:26 +0800

    update 12/28

---
+++ aspnet/MiniStore/WriteUrl.aspx.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace MiniStore
+{
+    public partial class WriteUrl : System.Web.UI.Page
+    {
+        JDB Main = new JDB();
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                if (Request["From"] != null && Request["To"] != null)
+                {
+                    Main.ParaClear();
+                    Main.ParaAdd("@FromUrl",Request["From"],System.Data.SqlDbType.NVarChar);
+                    Main.ParaAdd("@ToUrl",Request["To"],System.Data.SqlDbType.NVarChar); 
+                    Main.NonQuery("insert into UrlRefer (FromUrl,ToUrl,Date) values (@FromUrl,@ToUrl,getdate())"); 
+                }
+            }
+        }
+    }
+}
